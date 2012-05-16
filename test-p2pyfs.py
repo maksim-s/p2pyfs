9bd3d4a5485c1bb08c3594312bfc9a56463f1bd8
Usman Masood <usmanm@mit.edu>
2012-05-16 17:42:03 -0400

    re-hack

--- test-p2pyfs.py
+++ test-p2pyfs.py
@@ -1,13 +1,28 @@
 #!/usr/bin/python
-import os, threading, sys
+import os, threading, sys, time
 
 def create(name, prefix):
     n = "%s/%s"
     FILE = open(n, "w")
     FILE.close()
 
-def write(n):
-    pass
+def write(name, prefix, nf):
+    for i in range(nf):
+        n = "%s/%s-%d" % (name, prefix, i)
+        FILE = open(n, "w")
+        FILE.write("x")
+        FILE.close()
+
+def write_wrapper(name, prefix, nf, result, index):
+    write(name, prefix, nf)
+    result[index] = 1
+
+def checksize(name, prefix, i, size):
+    n = "%s/%s-%d" % (name, prefix, i)
+    if os.path.getsize(n) == size:
+        return True
+    return False
+
 def createn(name, prefix, nf):
     for i in range(nf):
         n = "%s/%s-%d" % (name, prefix, i)
@@ -26,6 +41,7 @@ def checkn_wrapper(name, prefix, nf, result, index):
 
 def test1(n_clients, files):
     print "Test 1: 1 client writes files, a lot of other clients read them"
+    t_initial = time.time()
     createn(files[0], "aa", 50)
     results = range(n_clients)
     threads = []
@@ -35,15 +51,17 @@ def test1(n_clients, files):
         threads.append(t)
     for t in threads:
         t.join()
+    t_final = time.time()
+    t_diff = t_final - t_initial
     if sum(results) == len(results):
-        print "Test 1: OK!"
+        print "Test 1: OK! Took: %f secs" % float(t_diff)
     else:
         print "Test 1: Failure!"
-
 
 def test2(n_clients, files):
     print "Test 2: All clients make files and read them"
     threads = []
+    t_initial = time.time()
     n_clients %= 3
     for i in range(n_clients):
         t = threading.Thread(target = createn, args=(files[i], "aa" + str(i), 50))
@@ -59,17 +77,31 @@ def test2(n_clients, files):
             threads.append(t)
     for t in threads:
         t.join()
+    t_final = time.time()
+    t_diff = t_final - t_initial
     if sum(results) == len(results):
-        print "Test 2: OK!"
+        print "Test 2: OK! Took: %f secs" % float(t_diff)
     else:
         print "Test 2: Failure!"
 
-
 def test3(n_clients, files):
     print "Test 3: One file, all clients write to it"
-    #creatn(files)
-    pass
-
+    t_initial = time.time()
+    createn(files[0], "zz", 50)
+    results = range(n_clients)
+    threads = []
+    for i in range(n_clients):
+        t = threading.Thread(target = write_wrapper, args=(files[i], "zz", 50, results, i))
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+    t_final = time.time()
+    t_diff = t_final - t_initial
+    if sum(results) == len(results) and checksize(files[0], "zz", 0, 1):
+        print "Test 3: OK! Took: %f secs" % float(t_diff)
+    else:
+        print "Test 3: Failure!"
 
 n_clients = 2
 if len(sys.argv) == 2:
@@ -81,4 +113,9 @@ for i in range(n_clients):
     files.append(os.path.abspath("yfs%d/" % i))
 
 test1(n_clients, files)
+print ""
 test2(n_clients, files)
+print ""
+test3(n_clients, files)
+print ""
+print "All tests completed!"
