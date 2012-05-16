71cc47debf9778beb5c3ff901dc0d5a0a3b030d6
Usman Masood <usmanm@mit.edu>
2012-05-16 08:32:19 -0400

    fix on usman's machine

--- start.sh
+++ start.sh
@@ -91,8 +91,9 @@ x=0
 for YFSDIR in ${YFS_DIRS[@]}
 do
     echo "Checking $YFSDIR"
-    if [ `mount | grep "$YFSDIR" | grep -v grep | wc -l` -ne 1 ]; then
-	sh stop.sh
+    pwd=`pwd -P`
+    if [ `mount | grep "$pwd/yfs$x" | grep -v grep | wc -l` -ne 1 ]; then
+	./stop.sh
 	echo "Failed to mount YFS properly at ./yfs$x"
 	exit -1
     fi
